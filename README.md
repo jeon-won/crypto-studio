ac83ee1726290c48e3a3b7d70c2b58ce8f17fab5
Jeon Won <j2onwon@gmail.com>
2025-05-26 01:58:01 +0900

    test-query-ai.py 코딩 중

--- README.md
+++ README.md
@@ -8,19 +8,9 @@
 
 ### 최상위 경로
 
-`.env`: 환경변수
+`insert_candle_data.py`: 현재와 바로 직전 시점의 비트코인 캔들 데이터(시간, 시가, 고가, 저가, 종가, 거래량, RSI) 저장
 
-```env
-POSTGRES_HOST = 
-POSTGRES_PORT = 
-POSTGRES_USER = 
-POSTGRES_PASSWORD = 
-POSTGRES_DB = 
-```
-
-`insert_current_candle_data.py`: 현재 시점의 비트코인 캔들 데이터(시간, 시가, 고가, 저가, 종가, 거래량, RSI)를 DB에 저장합니다.
-
-`insert_prev_candle_data.py`: 직전 시점의 비트코인 캔들 데이터(시간, 시가, 고가, 저가, 종가, 거래량, RSI)를 DB에 저장합니다.
+`test-query-ai.py(코딩 중)`: DB에 저장된 비트코인 데이터를 꺼내온 후 OpenAI에 질의
 
 ### docker
 
@@ -31,3 +21,8 @@ POSTGRES_DB =
 `aux_indicator.py`는 보조지표 관련 모듈
 
   * `calculate_rsi()`: RSI 값을 반환하는 함수
+
+`messenger.py`는 특정 메신저로 메시지를 보내는 모듈
+
+  * `send_telegram_message()`: 텔레그램 메시지 전송
+  * `send_discord_message()`: 디스코드 메시지 전송
--- test-query-ai.py
+++ test-query-ai.py
@@ -1,11 +1,9 @@
-from openai import OpenAI
 from dotenv import load_dotenv
-from module.aux_indicator import calculate_rsi
-import os
-import psycopg2
-import pandas as pd
-from pprint import pprint
 from module.messenger import send_discord_message
+from openai import OpenAI
+import os
+import pandas as pd
+import psycopg2
 
 # 상수 --------------------------------------------------
 
@@ -23,12 +21,10 @@ DISCORD_WEBHOOK_1D_ANALYSIS = os.getenv("DISCORD_WEBHOOK_1D_ANALYSIS")
 prompt = """You're a crypto trading expert. Please analyze the attached JSON data and answer the request below.
 
 - Can I buy bitcoin position(Long or Short) now?
-- Is there currently RSI bearish or bullish divergence?
+- Is there a RSI bearish or bullish divergence now?
 
 Please translate the answer into Korean.
 """
-
-# prompt = "You're a crypto trading expert. Please analyze the attached JSON data and let me know when the rsi divergence occurred. Translate the answer into Korean."
 conn_info = {
     'dbname': POSTGRES_DB,
     'user': POSTGRES_USER,
@@ -37,7 +33,7 @@ conn_info = {
     'port': POSTGRES_PORT
 }
 postgres_table = "btc_1d"
-rows_limit = 200  ## 가져올 데이터의 개수
+rows_limit = 100  ## 가져올 데이터의 개수
 
 # 코드 --------------------------------------------------
 
@@ -51,15 +47,13 @@ if __name__ == "__main__":
 
         # 가져온 데이터를 Pandas.DataFrame으로 변환 후 JSON으로 변환
         df = pd.DataFrame(rows, columns=[desc[0] for desc in cur.description])
-        df['time'] = df['time'].dt.tz_convert('Asia/Seoul')     ## 타임존 변환
-        df['time'] = df['time'].dt.strftime('%Y-%m-%d')   ## 또는 %Y-%m-%d %H:%M
-        df['open'] = df['open'].astype(int)                     ## OHLCV 값을 정수로 변환(OpenAI 질의 시 토큰 수 절약하기 위함)
+        df['time'] = df['time'].dt.tz_convert('Asia/Seoul')  ## 타임존 변환
+        df['time'] = df['time'].dt.strftime('%Y-%m-%d')      ## 또는 %Y-%m-%d %H:%M
+        df['open'] = df['open'].astype(int)                  ## OHLCV 값을 정수로 변환(OpenAI 질의 시 토큰 수 절약하기 위함)
         df['high'] = df['high'].astype(int)
         df['low'] = df['low'].astype(int)
         df['close'] = df['close'].astype(int)
         df['volume'] = df['volume'].astype(int)
-
-        print(df.to_json(orient='records', date_format='iso'))
 
     except psycopg2.DatabaseError as e:
         print(f"Database error: {e}")
@@ -71,43 +65,43 @@ if __name__ == "__main__":
         cur.close()
         conn.close()
 
-    # # OpenAI에 질의하기
-    # client = OpenAI(api_key=OPENAI_API_KEY)
-    # response = client.responses.create(
-    #   model="o4-mini",
-    #   input=[
-    #     {
-    #       "role": "developer",
-    #       "content": [
-    #         {
-    #           "type": "input_text",
-    #           "text": prompt
-    #         }
-    #       ]
-    #     },
-    #     {
-    #       "role": "user",
-    #       "content": [
-    #         {
-    #           "type": "input_text",
-    #           "text": df.to_json(orient='records', date_format='iso')
-    #         }
-    #       ]
-    #     },
-    #     # {
-    #     #   "role": "assistant",
-    #     #   "content": [
-    #     #     {
-    #     #       "type": "output_text",
-    #     #       "text": "Long"
-    #     #     }
-    #     #   ]
-    #     # },
-    #   ],
-    #   reasoning={
-    #     "effort": "medium"
-    #   },
-    # )
+    # OpenAI에 질의하기
+    client = OpenAI(api_key=OPENAI_API_KEY)
+    response = client.responses.create(
+      model="o4-mini",
+      input=[
+        {
+          "role": "developer",
+          "content": [
+            {
+              "type": "input_text",
+              "text": prompt
+            }
+          ]
+        },
+        {
+          "role": "user",
+          "content": [
+            {
+              "type": "input_text",
+              "text": df.to_json(orient='records', date_format='iso')
+            }
+          ]
+        },
+        # {
+        #   "role": "assistant",
+        #   "content": [
+        #     {
+        #       "type": "output_text",
+        #       "text": "Long"
+        #     }
+        #   ]
+        # },
+      ],
+      reasoning={
+        "effort": "medium"
+      },
+    )
 
-    # print(response.output[1].content[0].text)
-    # send_discord_message(DISCORD_WEBHOOK_1D_ANALYSIS, response.output[1].content[0].text)
+    print(response.output[1].content[0].text)
+    send_discord_message(DISCORD_WEBHOOK_1D_ANALYSIS, response.output[1].content[0].text)
