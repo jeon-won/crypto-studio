91daaf7936a998b3074965f9e3f5bbb59f832d3f
Jeon Won <j2onwon@gmail.com>
2025-05-26 20:39:54 +0900

    insert_candle_data.py에 argparse 코드 추가

--- insert_candle_data.py
+++ insert_candle_data.py
@@ -2,9 +2,7 @@
 현재와 직전 시점의 비트코인 캔들 데이터를 PostgreSQL DB에 저장합니다.
 저장하는 데이터는 시간, 시가, 고가, 저가, 종가, 거래량, RSI 값입니다.
 """
-import ccxt
-import os
-import psycopg2
+import argparse, ccxt, os, psycopg2, sys
 from datetime import datetime, timezone, timedelta
 from dotenv import load_dotenv
 from module.aux_indicator import calculate_rsi
@@ -21,13 +19,30 @@ POSTGRES_DB = os.getenv("POSTGRES_DB")
 # 전역변수 -----------------------------------------------
 
 symbol = "BTC/USDT"
-timeframe = "15m"
 time_zone = timezone(timedelta(hours=9))  ## GMT+9: 한국 시간
-postgres_table = "btc_15m"
+timeframe = ""       ## 5m, 15m, 1h, 4h, 1d 등(아래 argparse 코드에 의해 값이 결정됨)
+postgres_table = ""  ## btc_{timeframe} 형태(아래 argparse 코드에 의해 값이 결정됨)
 
 # 코드 --------------------------------------------------
 
 if __name__ == "__main__":
+    # argparse 설정
+    parser = argparse.ArgumentParser(
+        prog="insert_candle_data.py",
+        usage="%(prog)s 15m|1h|1d 와 같은 형식으로 입력해주세요"
+    )
+    parser.add_argument("timeframe", help="TimeFrame")
+    ## args가 비어있을 때 에러 처리
+    parser.error = lambda msg: (
+        parser.print_usage(sys.stderr),
+        sys.stderr.write(f"에러: {msg}\n"),
+        sys.exit(2)
+    )
+    args = parser.parse_args()
+    ## 전역변수 값 설정
+    timeframe = args.timeframe
+    postgres_table = f"btc_{args.timeframe}"
+
     # CCXT 및 PostgreSQL 관련 객체 초기화
     exchange = ccxt.binance({
         'options': {
@@ -43,7 +58,7 @@ if __name__ == "__main__":
     )
     cur = conn.cursor()
 
-    # 캔들 데이터 가져오기
+    # 캔들 데이터(Open, High, Low, Close, Volume) 가져오기
     ohlcvs = exchange.fetch_ohlcv(symbol, timeframe=timeframe, limit=201)
     ohlcvs_current = ohlcvs[1:] ## 현재 시점의 캔들 RSI 계산을 위한 배열
     ohlcvs_prev = ohlcvs[:-1]   ## 직전 시점의 캔들 RSI 계산을 위한 배열
@@ -59,13 +74,13 @@ if __name__ == "__main__":
         calculate_rsi(ohlcvs_prev)  ## RSI 값
     ]
     ohlcv_current = [
-        datetime.fromtimestamp(ohlcvs_current[-1][0] / 1000, tz=time_zone),  ## 시간(GMT+9 기준)
-        ohlcvs_current[-1][1],         ## 시가
-        ohlcvs_current[-1][2],         ## 고가
-        ohlcvs_current[-1][3],         ## 저가
-        ohlcvs_current[-1][4],         ## 종가
-        ohlcvs_current[-1][5],         ## 거래량
-        calculate_rsi(ohlcvs_current)  ## RSI 값
+        datetime.fromtimestamp(ohlcvs_current[-1][0] / 1000, tz=time_zone),
+        ohlcvs_current[-1][1], 
+        ohlcvs_current[-1][2], 
+        ohlcvs_current[-1][3], 
+        ohlcvs_current[-1][4], 
+        ohlcvs_current[-1][5], 
+        calculate_rsi(ohlcvs_current)
     ]
 
     # DB 데이터 INSERT 쿼리(충돌 시 UPDATE 수행)
